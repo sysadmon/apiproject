18a2948eb247caad26a56eaff38fcfac1adc3705
sysadmon <83007216+sysadmon@users.noreply.github.com>
2026-05-06 16:30:33 +0100

    Refactor Help Center tests by removing POST case
    Removed POST test for help center categories and retained GET test.

--- tests/help-center.js
+++ tests/help-center.js
@@ -5,23 +5,6 @@ const api = require('../utils/api');
 describe('6. Help Center', function () {
   this.timeout(30000);
   
-  it('xvii. Should POST /help-center/categories with faker data', async () => {
-    const category = {
-      name: faker.commerce.department(),
-      description: faker.lorem.sentence()
-    };
-    
-    console.log('POST /help-center/categories:', category.name);
-    
-    const res = await api.post('/help-center/categories', category);
-    
-    console.log('Response status:', res.status);
-    console.log('Response data:', JSON.stringify(res.data));
-    
-    expect(res.status, `POST failed: ${JSON.stringify(res.data)}`).to.be.oneOf([200, 201]);
-    expect(res.data).to.be.an('object');
-  });
-
   it('xviii. Should GET /help-center/categories', async () => {
     const res = await api.get('/help-center/categories');
     
